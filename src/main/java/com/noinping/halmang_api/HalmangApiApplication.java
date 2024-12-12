3cde77424fc2b08671e19a44a5133c4fe7e06336
jeongyeonho <fnelclsrn123@gmail.com>
2024-12-12 14:05:49 +0900

    chore: 쿠버네티스 설정 파일

---
+++ src/main/java/com/noinping/halmang_api/HalmangApiApplication.java
@@ -0,0 +1,13 @@
+package com.noinping.halmang_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalmangApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalmangApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noinping/halmang_api/HalmangApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noinping.halmang_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalmangApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
