56805906cb57920eee34b0218a32114c560363ce
jeongyeonho <fnelclsrn123@gmail.com>
2024-12-12 13:56:23 +0900

    feat: stt 기능 생성

---
+++ src/main/java/com/noinping/halmang_api/domain/sts/controller/SttController.java
@@ -0,0 +1,27 @@
+package com.noinping.halmang_api.domain.sts.controller;
+
+import com.noinping.halmang_api.domain.claude.model.response.ContentsResponse;
+import com.noinping.halmang_api.domain.sts.service.SttService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class SttController {
+
+    private final SttService sttService;
+
+    @PostMapping(value = "/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ContentsResponse handleAudioMessage(@RequestParam("audioFile") MultipartFile audioFile) throws IOException {
+        return sttService.transcribe(audioFile, 44100);
+
+    }
+
+
+}
